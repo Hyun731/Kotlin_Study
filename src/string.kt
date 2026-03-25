359dcd7c21ad6303c5af113ff57bd158d8a1f8c7
hyun731 <gilhyun.gwon0731@gmail.com>
2026-03-26 00:10:23 +0900

    String

---
+++ src/string.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var name = "hEllo"
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name[0])
+    name = "gilhyun"
+    println("my name is " + name)
+    println("my name is $name 입니다.")
+    println("my name is ${name + 10}입니다.")
+}
