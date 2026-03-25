34f8e778229be2d61dcf7cac49db6794ebf5176c
hyun731 <gilhyun.gwon0731@gmail.com>
2026-03-25 23:15:48 +0900

    변수

---
+++ src/main.kt
@@ -0,0 +1,3 @@
+fun main() { //main 함수가 있어야 지만 실행이 가능함
+    print("Hello World") //세미콜론을 기본적으로 붙이지 않음 println도 존재함
+}
---
+++ src/var.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //코틀린은 기본적으로 타입 추론이 있기 때문에 타입을 선언하지 않아도 됨
+    var i = 10
+    var name = "Gilhyun"
+    var point = 1.0
+    //하지만 타입을 명시적으로 선언하고 싶은 경우 아래와 같이 선언하면 됨
+    //타입은 참조 타입을 가지고 있음
+    var ii : Int = 0
+    var name1 : String = "Gilhyun"
+    var point2 : Double = 1.0
+
+}
